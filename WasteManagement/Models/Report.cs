be4719b4872dd7256184dd07944c059459ad01c2
Mbali Sibiya <Mesibiya13@gmail.com>
2026-05-23 14:51:51 +0200

    Report class

---
+++ WasteManagement/Models/Report.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace WasteManagement.Models
+{
+    public class Report
+    {
+       
+            public int ReportId { get; set; }
+
+            [Required]
+            public string? Title { get; set; }
+
+            [Required]
+            public string? Description { get; set; }
+
+            public string? Location { get; set; }
+
+            public string? ImageUrl { get; set; }
+
+            public string Status { get; set; } = "Pending";
+
+            public DateTime DateReported { get; set; } = DateTime.Now;
+        
+    }
+}
+
